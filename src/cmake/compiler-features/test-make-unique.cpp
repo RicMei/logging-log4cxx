69512f0d096d456f7738e417a3fe51face17b48a
work_msh@126.com <Fmeishuhao13>
2024-06-21 17:55:13 +0800

    removeall

--- src/cmake/compiler-features/test-make-unique.cpp
+++
@@ -1,6 +0,0 @@
-#include <memory>
-
-int main(int argc, char** argv){
-	std::make_unique<int>(5);
-	return 0;
-}
--- src/cmake/pthread/test-pthread-getname.cpp
+++
@@ -1,7 +0,0 @@
-#include <pthread.h>
-
-int main(){
-	pthread_t tid;
-	char buffer[16];
-	pthread_getname_np(tid, buffer, sizeof(buffer));
-}
